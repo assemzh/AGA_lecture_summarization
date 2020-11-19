cb06c71b2495baa4e8a82b13d95ed13650f124f5
Assem Zhunis <zhunis.assem@gmail.com>
2020-11-20 00:25:31 +0600

    implement wikipedia search

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,7 @@
 # from textblob import TextBlob
 from flask_cors import CORS
 from summarizer import Summarizer
+import wikipedia
 # TextBlob(sentence).sentiment
 
 from flask import Flask, jsonify, request
@@ -8,13 +9,25 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/flask-backend/summary', methods=['POST'])
-def say_hello_world():
+def summarize():
     data = request.get_json()
     body = data["fullText"]
     model = Summarizer()
     result = model(body, ratio=data['detail_level'])  # Specified with ratio
     return jsonify({"result":result})
 
+
+#Search Wikipedia
+@app.route('/flask-backend/wiki', methods=['POST'])
+def find_wiki():
+    data = request.get_json()
+    keyword = data["selectedWord"]
+    try:
+        result = wikipedia.summary(keyword, sentences=1)
+        page = wikipedia.page(keyword)
+        return jsonify({"result": result, "url": page.url, "keyword": keyword})
+    except:
+        return jsonify({"result": None, "url": None, "keyword": None})
 
 @app.route('/', methods=['GET'])
 def hello():
